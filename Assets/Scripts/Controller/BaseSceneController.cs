8b0d43c8b490787b096867b3c77c58abc1c4c020
Modin <eom3658@gmail.com>
2025-04-13 18:01:34 +0900

    SceneController, SceneConfig

---
+++ Assets/Scripts/Controller/BaseSceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    /// <summary>
+    /// Scene에 대한 제어 및 관리를 담당한다.
+    /// </summary>
+    public class BaseSceneController : MonoBehaviour
+    {
+        
+    }
+}
