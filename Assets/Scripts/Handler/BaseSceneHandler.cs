bd69d193bb8bdb08fe7f74377118c6cb1316e7e9
Modin <eom3658@gmail.com>
2025-04-13 11:14:05 +0900

    SceneHandler 개발

---
+++ Assets/Scripts/Handler/BaseSceneHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Handler
+{
+    /// <summary>
+    /// Scene에 대한 자원 관리를 합니다.
+    /// </summary>
+    public class BaseSceneHandler : MonoBehaviour
+    {
+        
+    }
+}
