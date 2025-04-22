46b194000be4f6af740d717b593166b47929bed1
Modin <eom3658@gmail.com>
2025-04-22 21:46:11 +0900

    계정 정보 데이터 구조 선언

---
+++ Assets/Scripts/Define/CommonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Define
+{
+    [Serializable]
+    public class AccountData
+    {
+        
+    }
+
+    [Serializable]
+    public class EmailAccountData : AccountData
+    {
+        public string email;
+        public string password;
+    }
+
+    [Serializable]
+    public class GoogleAccountData : AccountData
+    {
+        public string refreshToken;
+    }
+}
