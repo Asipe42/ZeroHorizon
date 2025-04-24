0ebd02b32895c4f131944783e166f01c33901f85
Modin <eom3658@gmail.com>
2025-04-24 13:43:15 +0900

    RoomManager

---
+++ Assets/Scripts/Manager/RoomManager.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Manager
+{
+    public class RoomManager : MonoBehaviourPunCallbacks
+    {
+        public void ConnectMaster()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public void JoinRoom(string roomName)
+        {
+            PhotonNetwork.CreateRoom(roomName);
+        }
+
+        public void CreateRoom(string roomName)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+            
+            Debug.Log("OnConnectedToMaster");
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            base.OnJoinedLobby();
+            
+            Debug.Log("OnJoinedLobby");
+        }
+    }
+}
