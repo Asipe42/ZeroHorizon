cb12a72b62272fbd22450cfa04faa15809a4c30f
Modin <eom3658@gmail.com>
2025-04-29 11:59:30 +0900

    플레이어 이동

--- Assets/Scripts/Actor/PlayerCharacter.cs
+++ Assets/Scripts/Actor/PlayerCharacter.cs
@@ -9,6 +9,9 @@ namespace Actor
     {
         [FoldoutGroup("PUN2")] [SerializeField] private PhotonView photonView;
         [FoldoutGroup("PUN2")] [SerializeField] private PhotonRigidbodyView photonRigidbodyView;
+        
+        [SerializeField] private float speed;
+        [SerializeField] private float maxVelocityChange;
 
         [SerializeField] private Rigidbody rigid;
 
@@ -32,7 +35,34 @@ namespace Actor
             base.CustomUpdate();
             _input.Normalize();
             
-            Debug.Log($"Input: {_input}");
+        }
+
+        public override void CustomFixedUpdate()
+        {
+            base.CustomFixedUpdate();
+            rigid.AddForce(CalcMovement(), ForceMode.VelocityChange);
+        }
+
+        private Vector3 CalcMovement()
+        {
+            Vector3 targetVelocity = new Vector3(_input.x, 0, _input.y);
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+            
+            Vector3 nowVelocity = rigid.linearVelocity;
+            if (_input.magnitude > 0.5f)
+            {
+                Vector3 changedVelocity = targetVelocity - nowVelocity;
+                changedVelocity.x = Mathf.Clamp(changedVelocity.x, -maxVelocityChange, maxVelocityChange);
+                changedVelocity.z = Mathf.Clamp(changedVelocity.z, -maxVelocityChange, maxVelocityChange);
+                changedVelocity.y = 0;
+
+                return changedVelocity;
+            }
+            else
+            {
+                return new Vector3();
+            }
         }
 
         private void OnInputHorizontal(float value)
